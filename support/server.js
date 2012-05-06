99047688e93db676c31038f87703548ee5a74801
Jake Luer <jake.luer@gmail.com>
2012-05-06 03:45:18 -0400

    test server now has request logging

--- support/server.js
+++ support/server.js
@@ -3,21 +3,26 @@ var connect = require('connect')
   , http = require('connect')
   , join = require('path').join;
 
-
 var proxy = carbon.listen(5000)
-  , app = connect();
+  , log = carbon.logger('sc-test');
 
+log
+  .use(carbon.logger.console())
+  .start();
 
-proxy.use(function (req, res, next) {
-  var url = req.url.split('/');
-  if (url[1] === '_couchdb') {
-    req.url = '/' + url.slice(2).join('/');
-    return next(5984);
-  }
-  next(5001);
-});
+proxy
+  .use(log.middleware())
+  .use(function (req, res, next) {
+    var url = req.url.split('/');
+    if (url[1] === '_couchdb') {
+      req.url = '/' + url.slice(2).join('/');
+      return next(5984);
+    }
+    next(5001);
+  });
 
+var app = connect()
 app.use(connect.static(join(__dirname, '..')));
 http.createServer(app).listen(5001);
 
-console.log('supercouch test server running on port %d', proxy.server.address().port);
+console.log('supercouch test server running on port %d\n', proxy.server.address().port);
