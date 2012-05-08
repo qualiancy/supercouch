67869dc47a4392dac17c87d5579b2c60129ae550
Veselin Todorov <veselin@veselin.bg>
2012-05-08 21:10:48 +0300

    Better tests and more docs

--- test/supercouch.js
+++ test/supercouch.js
@@ -8,6 +8,21 @@ var expect = chai.expect;
 describe('SuperCouch', function () {
   var couch = supercouch('http://local.host:5000/_couchdb');
 
+  function createDb(name, cb) {
+    couch
+      .dbExists(name)
+      .end(function (err, exists) {
+        if (exists) return cb();
+        couch.dbAdd(name).end(cb);
+      });
+  };
+
+  function clean(name, cb) {
+    couch.dbDel(name).end(cb);
+  };
+
+  // TODO: DB cleanup after tests
+
   it('has a version', function () {
     expect(supercouch).to.have.property('version');
   });
@@ -23,9 +38,8 @@ describe('SuperCouch', function () {
       });
   });
 
-  describe('database management', function () {
-
-    it('can create a new db', function (done) {
+  it('can create a new db', function (done) {
+    clean('sc_dbmgmt', function() {
       couch
         .dbAdd('sc_dbmgmt')
         .end(function (err, res) {
@@ -34,34 +48,51 @@ describe('SuperCouch', function () {
           done();
         });
     });
+  });
 
-    it('can get the info for a db', function (done) {
-      couch
-        .dbInfo('sc_dbmgmt')
-        .end(function (err, res) {
-          expect(err).to.not.exist;
-          expect(res).to.be.a('object')
-            .and.to.have.property('db_name', 'sc_dbmgmt');
-          done();
-        });
-    });
+  it('can get the info for a db', function (done) {
+    couch
+      .dbInfo('sc_dbmgmt')
+      .end(function (err, res) {
+        expect(err).to.not.exist;
+        expect(res).to.be.a('object')
+          .and.to.have.property('db_name', 'sc_dbmgmt');
+        done();
+      });
+  });
 
-    it('can check if a db exist', function(done) {
-      couch
-        .dbExist('sc_dbmgmt')
-        .end(function (err, res) {
-          expect(err).to.not.exist;
-          expect(res).to.be.true;
-          done();
-        });
-    });
-
-    it('can delete a db', function (done) {
+  it('can delete a db', function (done) {
+    createDb('sc_dbmgmt', function() {
       couch
         .dbDel('sc_dbmgmt')
         .end(function (err, res) {
           expect(err).to.not.exist;
           expect(res).eql({ ok: true });
+          done();
+        });
+    });
+  });
+
+  describe('db existance', function() {
+
+    it('can tell if a db exists', function(done) {
+      createDb('sc_dbmgmt', function() {
+        couch
+          .dbExists('sc_dbmgmt')
+          .end(function (err, res) {
+            expect(err).to.not.exist;
+            expect(res).to.be.true;
+            done();
+          });
+      });
+    });
+
+    it('can tell if db does not exist', function(done) {
+      couch
+        .dbExists('this-is-unreal')
+        .end(function (err, res) {
+          expect(err).to.not.exist;
+          expect(res).to.be.false;
           done();
         });
     });
