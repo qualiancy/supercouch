257e9d3a0f6f72d193eb462c470c62ad0bd4fad0
Jake Luer <jake.luer@gmail.com>
2012-06-02 18:42:31 -0400

    clean up tests

--- test/supercouch.js
+++ test/supercouch.js
@@ -3,63 +3,57 @@ if (!chai) {
     , supercouch = require('..');
 }
 
-/**
- * Support
- *
- * @type {Function}
- */
 var expect = chai.expect;
 
-/**
- * Test database
- *
- * @type {String}
- */
-var database = 'sc_dbmgmt';
-
-/**
- * Ensure database.
- *
- * @param {String} name
- * @param {Function} callback
- */
-function ensureDb(name, cb) {
-  couch
-    .dbExists(name)
-    .end(function (err, exists) {
-      if (exists) return cb();
-      couch.dbAdd(name).end(cb);
-    });
-};
-
-/**
- * Remove database.
- *
- * @param {String} name
- * @param {Function} callback
- */
-function removeDb(name, cb) {
-  couch.dbDel(name).end(cb);
-};
-
-// TODO: Add custom Chai.js matchers.
-//
-// Idea:
-//
-//    expect(record).to.be.deleted.from(db);
-//    expect(record).to.exist.in(db);
-
-/**
- * Subject
- *
- * @type {Supercouch}
- */
-var couch = supercouch('http://local.host:5000/_couchdb');
-
 describe('SuperCouch', function () {
+  var couch
+    , database = 'sc_dbmgmt';
 
-  beforeEach(function(done) {
+  /**
+   * Ensure database.
+   *
+   * @param {String} name
+   * @param {Function} callback
+   */
+
+  function ensureDb(name, cb) {
+    couch
+      .dbExists(name)
+      .end(function (err, exists) {
+        if (exists) return cb();
+        couch
+          .dbAdd(name)
+          .end(function (err, res) {
+            cb();
+          });
+      });
+  };
+
+  /**
+   * Remove database.
+   *
+   * @param {String} name
+   * @param {Function} callback
+   */
+
+  function removeDb(name, cb) {
+    couch
+      .dbDel(name)
+      .end(function (err, res) {
+        cb();
+      });
+  };
+
+  before(function () {
+    couch = supercouch('http://local.host:5000/_couchdb');
+  });
+
+  beforeEach(function (done) {
     ensureDb(database, done);
+  });
+
+  after(function (done) {
+    removeDb(database, done);
   });
 
   // TODO: DB cleanup after tests
@@ -112,13 +106,24 @@ describe('SuperCouch', function () {
       });
   });
 
-  it('can create a new record', function(done) {
+  it('can create a new record with id', function(done) {
     couch
       .db(database)
       .insert({ _id: '1', foo: 'bar' })
       .end(function (err, res) {
         expect(err).to.not.exist;
         expect(res.ok).to.be.true;
+        done();
+      });
+  });
+
+  it('can create a new record without and id', function (done) {
+    couch
+      .db(database)
+      .insert({ foo: 'baz' })
+      .end(function (err, res) {
+        expect(err).to.not.exist;
+        expect(res).to.have.property('ok', true);
         done();
       });
   });
